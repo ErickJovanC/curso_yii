6aae94fd8f50c477a92cefa63e5bcdb89a8f7811
Erick Jovan <erickjovan106@gmail.com>
2024-05-17 20:39:00 -0600

    Personalización de Formularios

--- views/product/_form.php
+++ views/product/_form.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use app\models\Category;
 use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
@@ -8,32 +9,69 @@ use yii\widgets\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="product-form">
-
+<div class="product-form row">
+    <div class="col-md-6">
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'title')
+        ->textInput([
+            'maxlength' => true,
+            'class' => 'mb-3 form-control',
+            'placeholder' => 'Name of Product',
+        ]) ?>
 
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'description')
+        ->textarea(['maxlength' => true, 'class' => 'form-control y mb-3']) ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')
+        ->dropDownList(
+            Category::find()->select(['title', 'id'])->indexBy('id')->column(),
+            ['class' => 'form-control mb-3',
+                'prompt' => ['text' => 'Select Category', 'options' => ['hidden' => true]]]
+        ) ?>
 
-    <?= $form->field($model, 'price')->textInput() ?>
+    <?= $form->field($model, 'price')
+        ->textInput([
+            'type' => 'number',
+            'step' => '0.5',
+            'class' => 'form-control mb-3'
+        ]) ?>
 
-    <?= $form->field($model, 'cost')->textInput() ?>
+    <?= $form->field($model, 'cost')
+        ->textInput([
+            'type' => 'number',
+            'step' => '0.5',
+            'class' => 'form-control mb-3'
+        ]) ?>
 
-    <?= $form->field($model, 'stock')->textInput() ?>
+    <?= $form->field($model, 'stock')
+        ->textInput([
+            'type' => 'number',
+            'class' => 'form-control mb-3'
+        ]) ?>
 
-    <?= $form->field($model, 'is_countable')->textInput() ?>
+    <?= $form->field($model, 'is_countable')
+        ->radioList(
+            [1 => 'Si', 0 => 'No'],
+            ['class' => 'form-check mb-3',
+                'separator' => ' / ',
+                'itemOptions' => ['class' => 'form-check-input mx-1']]
+        ) ?>
 
-    <?= $form->field($model, 'bulk')->textInput() ?>
+    <?= $form->field($model, 'bulk')
+        ->checkbox(['class' => 'form-check-input mb-3']) ?>
 
-    <?= $form->field($model, 'sku')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'sku')
+        ->textInput([
+            'maxlength' => true,
+            'type' => 'number',
+            'class' => 'form-control mb-3'
+        ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
-
+    </div>
 </div>
